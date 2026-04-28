f428b9cc7dfc74b87740e634bb29f2519952e024
yuying.chen <yuying.chen@tum.de>
2026-04-28 17:33:47 +0200

    Add a regex to detect titles containing numbers

--- text_vorverarbeiter/.m/detectTocTitle.m
+++ text_vorverarbeiter/.m/detectTocTitle.m
@@ -1,6 +1,7 @@
-function matchTitle = detectTocTitle(line)
+function matchTitle = detectTocTitle(thisLine)
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
+    thisLine = lower(thisLine);
     matchTitle = false;
     keywords = {'content','contents','list of contents','table of contents','index of contents','toc','contents overview'...
         'inhalt','inhalte','verzeichnis','inhaltsverzeichnis'};
@@ -8,8 +9,8 @@ function matchTitle = detectTocTitle(line)
         % pattern = sprintf('^#* *%s *$',keywords{i});
         pattern = sprintf([ ...
             '^(?!#*\\s*(?:list of\\s+)?(?:tables|figures|images?|illustrations)\\s*$)' ...
-            '#*\\s*%s\\s*$'], keywords{i});
-        match = matchPattern(line,pattern);
+            '#*\\s*(?:\\d+(?:\\.\\d+)*\\.?\\s*)?%s\\s*$'], keywords{i});
+        match = matchPattern(thisLine,pattern);
         if match
             matchTitle = true;
             %disp(line); % test
